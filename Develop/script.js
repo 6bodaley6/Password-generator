9bd64cc3fd6d00af31f1885806135f9044c150c6
Bo Daley <70559889+6bodaley6@users.noreply.github.com>
2020-11-15 14:59:10 -0700

    saving before i completly overhaul javascript incase i need to come back

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,13 @@
 
 //array with numbers for characters,numbers,symbols//
 
-var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","n","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","n","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
-var numbers = ["1","2","3","4","5","6","7","8","9","10"]
+var numbers = ["1","2","3","4","5","6","7","8","9","10"];
 
-var symbol = ["!","@","#","$","%","^","&","*","(",")",]
+var symbol = ["!","@","#","$","%","^","&","*","(",")",];
 
-var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
 //if lowerCase you replace the first if uppercase you replace the 3rd if specialcharacter replace4 if number 
 
